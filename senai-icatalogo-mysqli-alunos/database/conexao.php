e87f6a606a97132d266c5dac3b46d6688351136a
STEPHANY DE SOUZA RAMOS <21192996@CFP127ALUNO.LOCAL>
2021-09-30 09:56:13 -0300

    novo projeto

---
+++ senai-icatalogo-mysqli-alunos/database/conexao.php
@@ -0,0 +1,23 @@
+<?php
+
+/* paremetros de conexão Mysqli
+
+1- host-> onde o banco esta rodando
+2 user-> usuario do banco de dados
+3 password -> senha do usuario do banco de date_isodate_set
+4- detabase -> nome do banco de dado */
+
+
+const host ='localhost';
+const user = 'root';
+const password ='bcd127';
+const dataBase ='icatalogo';
+
+$conexão = mysqli_connect (host, user, password, dataBase);
+if($conexão === false){
+    die(mysqli_connect_error());
+}
+
+echo'<pre>';
+var_dump($conexão);
+echo'</pre>';
