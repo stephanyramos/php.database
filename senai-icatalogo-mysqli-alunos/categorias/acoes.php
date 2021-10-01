42120f247fbcc592ba7bc9abf97590edb5c522b6
STEPHANY DE SOUZA RAMOS <21192996@CFP127ALUNO.LOCAL>
2021-10-01 11:17:21 -0300

    finalizando-1

--- senai-icatalogo-mysqli-alunos/categorias/acoes.php
+++ senai-icatalogo-mysqli-alunos/categorias/acoes.php
@@ -1,40 +1,91 @@
 <?php
-
-/* Conexão com  banco de dados */
+session_start();
+/*CONEXÃO COM BANCO DE DADOS*/
 require('../database/conexao.php');
-/* tratamentos doa dados vindos do formulario
--TIPOS DE AÇÕES 
--EXECULÇÃO DOS PROCESSOS DA AÇÃO SOLICITADA */
 
+/*FUNÇÃO DE VALIDAÇÃO*/
+function validaCampos(){
 
-switch($_POST['acao']) {
+    $erros = [];
+
+    if(!isset($_POST['descricao']) || $_POST['descricao'] == ""){
+
+        $erros[] = "O campo descrição é de preenchimento obrigatório";
+
+    }
+
+    return $erros;
+
+}
+
+/*
+TRATAMENTO DOS DADOS VINDOS DO FORMULÁRIO
+
+- TIPOS DA AÇÃO
+- EXECUÇÃO DOS PROCESSOS DA AÇÃO SOLICITADA
+
+*/
+switch ($_POST['acao']) {
+
     case 'inserir':
+
+        //CHAMADA DA FUNÇÃO DE VALIDAÇÃO DE ERROS:
+        $erros = validaCampos();
+
+        //VERIFICAR SE EXISTEM ERROS:
+        if(count($erros) > 0){
+
+            $_SESSION["erros"] = $erros;
+
+            header('location: index.php');
+
+            exit();
+
+        }
+
         // echo 'INSERIR';exit;
-    $descricao = $_POST['descricao'];
 
-    /*montagm da inscrição sql de inserção de dados*/
-    $sql ="INSERT INTO tbl_categoria (descricao) 
-    values('$descricao')";
+        $descricao = $_POST['descricao'];
 
-    // echo $sql; exit;
+        /*MONTGEM DA INSTRUÇÃO SQL DE INSERÇÃO DE DADOS:*/
+        $sql = "INSERT INTO tbl_categoria (descricao)
+        VALUES ('$descricao')";
 
+        // echo $sql;exit;
 
-    /*parametros
-    1- uma conexao abeta valida 
-    2- uma instrução sql*/
-    $resultado = mysqli_query($conexao, $sql);
-     header('location:index.php');
+        /*
+        mysql_query parametros:
+        1 - Uma conexão aberta e válida
+        2 - Uma instrução sql válida
+        */
+        $resultado = mysqli_query($conexao, $sql);
 
-    // echo'<pre>';
-    // var_dump($conexao);
-    // echo'</pre>';
-    // exit;
+        header('location:index.php');
+
+        // echo '<pre>';
+        // var_dump($resultado);
+        // echo '</pre>';
+        // exit;
 
         break;
+
+        case 'deletar':
+
+            $categoriaID = $_POST['categoriaId'];
+
+            $sql = "DELETE FROM tbl_categoria WHERE id = $categoriaID";
+
+            $resultado = mysqli_query($conexao, $sql);
+
+            header('location: index.php');
+
+            break;
     
     default:
         # code...
         break;
 }
+
+
 
 ?>
--- senai-icatalogo-mysqli-alunos/categorias/index.php
+++ senai-icatalogo-mysqli-alunos/categorias/index.php
@@ -1,18 +1,21 @@
 <?php
-   
+
+    session_start();
+
     require('../database/conexao.php');
 
     $sql = "SELECT * FROM tbl_categoria";
 
     $resultado = mysqli_query($conexao, $sql);
-    
-    // echo'<pre>';
-    // var_dump($conexão);
-    //  echo'</pre>';
-    
+
+    // $categoria = mysqli_fetch_array($resultado);
+
+    // echo '<pre>';
+    // var_dump($categoria);
+    // echo '</pre>';
+    // exit;
 
 ?>
-
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -35,9 +38,30 @@
                 <form class="form-categoria" method="POST" action="./acoes.php">
                     <input type="hidden" name="acao" value="inserir" />
                     <h1 class="span2">Adicionar Categorias</h1>
+                    
                     <ul>
+                    <?php
+                        if(isset($_SESSION["erros"])){
 
+                            foreach ($_SESSION["erros"] as $erro) {
+                                
+                    ?>
+
+                        <li><?php echo $erro?></li>
+
+                    <?php
+                        }//fim do foreach
+
+                        //Limpa a variável de sessão
+                        session_unset();
+                        
+                        //Destroi a sessão
+                        session_destroy();
+
+                    }//fim do if 
+                    ?>
                     </ul>
+
                     <div class="input-group span2">
                         <label for="descricao">Descrição</label>
                         <input type="text" name="descricao" id="descricao"/>
@@ -47,22 +71,19 @@
                 </form>
                 <h1>Lista de Categorias</h1>
 
-
                     <?php
-
-                    while($categoria = mysqli_fetch_array($resultado)){
-
-                        echo $categoria["descricao"];
-
-                    }
-
-                    exit;
+                    
+                        while($categoria = mysqli_fetch_array($resultado)){
 
                     ?>
 
                     <div class="card-categorias">
-                        <img onclick="deletar()" src="https://icons.veryicon.com/png/o/construction-tools/coca-design/delete-189.png" />
+                        <?php echo $categoria["descricao"]; ?>
+                        <img onclick="deletar(<?php echo $categoria['id']; ?>)" src="https://icons.veryicon.com/png/o/construction-tools/coca-design/delete-189.png" />
+                        <img onclick="javascript: window.location = 'editar.php?id=<?php echo $categoria['id']; ?>'" src="https://icons.veryicon.com/png/o/leisure/weight-with-linear-icon/edit-65.png" />
                     </div>
+
+                    <?php } ?>
 
                 <form id="form-deletar" method="POST" action="./acoes.php">
                     <input type="hidden" name="acao" value="deletar" />
