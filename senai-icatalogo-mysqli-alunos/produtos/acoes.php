053bb1517c23cf407511d8276a98b2b77de9d19a
STEPHANY DE SOUZA RAMOS <21192996@CFP127ALUNO.LOCAL>
2021-10-08 11:25:22 -0300

    finalizando

--- senai-icatalogo-mysqli-alunos/produtos/acoes.php
+++ senai-icatalogo-mysqli-alunos/produtos/acoes.php
@@ -23,7 +23,37 @@
             // echo '<br>';
             // echo $novoNome;
             move_uploaded_file($_FILES["foto"]["tmp_name"] , "fotos/$novoNome");
- 
+
+            /*INSTENÇÃO DE DADOS NA BASE DE DADOS DO MYSQL*/
+
+            //RECEBIMENTO DE DADOS 
+            $descricao = $_POST["descricao"];
+            $peso = $_POST["peso"];
+            $quantidade= $_POST ["quantidade"];
+            $cor = $_POST["cor"];
+            $tamanho = $_POST["tamanho"];
+            $valor = $_POST ["valor"];
+            $desconto = $_POST ["desconto"];
+            $categoriaId = $_POST["categoria"];
+            
+            //Descriçao da instrução  SQL inserção
+
+            $sql = " INSERT INTO  tbl_produto
+            (descricao, peso, quantidade, cor, tamanho, valor, desconto, imagem, categoria_Id)
+            VALUES('$descricao', $peso, $quantidade, '$cor', '$tamanho', $valor, $desconto,
+            '$novoNome', $categoriaId)";
+
+            // '' SERVE PARA SQL QUE REPRESNETA STRING
+
+            //EXECUÇÃO DO SQL DE INSERÇÃO
+
+            $resultado = mysqli_query($conexao, $sql);
+
+            //REDIRECIONAR PARA INDEX://
+
+        header('local: index.php');
+
+
             break;
         
         default:
